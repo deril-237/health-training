64097b7efcd8c60b1e0144a43766d7e8004179cd
deril-237 <deffofossoderil@gmail.com>
2026-08-26 14:26:33 +0000

    add loading component in public page

--- src/app/(public)/loading.tsx
+++ src/app/(public)/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="w-full h-full flex justify-center items-center">
-      <span className="loading loading-dots size-20 text-primary"></span>
+      <span className="loading loading-dots size-18 text-primary"></span>
     </div>
   );
 }
